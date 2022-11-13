715c70ffe7fc01a59d57f52ef2a2d194310eaa24
abehidek <hidek.abe@outlook.com>
2022-11-13 18:45:24 -0300

    web: sign up page and fetch user session in navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,12 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 
+const api = "http://localhost:4000/api"
+
 export default function Navbar() {
+  const auth = useQuery(["me"], () =>
+    fetch(api + '/users/me', {
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(res => res.json())
+  )
+  async function logout() {
+    const response = await fetch(api + '/users/logout', {
+      method: 'DELETE',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }
+    ).then(res => res.json())
+
+    auth.refetch()
+
+    console.log(response)
+  }
+
   return (
     <div className="flex gap-5">
       <h1><Link href="/">Abank</Link></h1>
       <div className="flex gap-5">
-        <h3><Link href="/login">Login</Link></h3>
-        <h3><Link href="/signup">Sign up</Link></h3>
+        {auth.data.user
+          ? (
+            <>
+            <h3>Hello {auth.data.user.email}</h3>
+            <h3><button onClick={logout}>Logout</button></h3>
+            </>
+          )
+          : (
+            <>
+            <h3><Link href="/login">Login</Link></h3>
+            <h3><Link href="/signup">Sign up</Link></h3>
+            </>
+          )
+        }
       </div>
     </div>
   )
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,23 @@
+import { useQuery } from "@tanstack/react-query"
 import { NextPage } from "next"
 import Base from "../layouts/Base"
 
+const api = "http://localhost:4000/api"
+
 const Home: NextPage = () => {
+  const auth = useQuery(["me"], () =>
+    fetch(api + '/users/me', {
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(res => res.json())
+  )
   return (
     <Base>
       Index
+      {JSON.stringify(auth.data, null, 2)}
     </Base>
   )
 }
