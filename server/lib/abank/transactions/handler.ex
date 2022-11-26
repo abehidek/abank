fdc4d7bcc373aa0d1619b2cfb6046e0d3a874b13
abehidek <hidek.abe@outlook.com>
2022-11-26 00:05:06 -0300

    finish: credit_transaction
    steps:
    - gets credit_card by card_number
    - gets current open invoce from credit_card
    - if open_invoice exists, it proceeds to transaction, else call Invoice handler verify credit card invoices status
    - if proceeding transaction, verify if transaction amount is greater than the limit minus current open invoice amount.
    - if transaction amount is greater, returns error message saying transaction surpass the credit card limit and rejects the transaction
    - else, increment current open invoice amount and increment to_account balance, and approves the transaction

--- server/lib/abank/transactions/handler.ex
+++ server/lib/abank/transactions/handler.ex
@@ -1,6 +1,9 @@
 defmodule Abank.Transactions.Handler do
   alias Abank.Accounts
   alias Abank.Transactions.Transaction
+  alias Abank.Invoices.Invoice
+  alias Abank.Invoices.Handler
+  alias Abank.Cards.Card
 
   def run_all_transactions() do
     # get all transactions with "open" status
@@ -22,7 +25,7 @@ defmodule Abank.Transactions.Handler do
     transaction.inserted_at
   end
 
-  defp normal_transaction(transaction, from_account, to_account) do
+  defp normal_transaction(%Transaction{} = transaction, from_account, to_account) do
     if Accounts.sufficient_balance?(transaction.amount_in_cents, from_account) do
       with {:ok, result} <-
              Abank.Repo.transaction(fn ->
@@ -59,13 +62,64 @@ defmodule Abank.Transactions.Handler do
     end
   end
 
-  defp credit_transaction(transaction, from_account, to_account) do
+  defp credit_transaction(%Transaction{} = transaction, _from_account, to_account) do
     # this needs to search the account invoice (needs a method for get invoices by account)
     # verify if the invoice amount is less than the account limit
-    IO.inspect(transaction)
-    IO.inspect(from_account)
-    IO.inspect(to_account)
-    {:ok, %{result: "Credit transaction successfully"}}
+
+    {:ok, card_by_number_query} = Card.get_card_by_number_query(transaction.card_number)
+
+    credit_card = card_by_number_query |> Abank.Repo.one()
+
+    {:ok, open_invoice_by_credit_card_query} =
+      Invoice.get_open_invoice_by_credit_card_query(credit_card)
+
+    open_invoice =
+      open_invoice_by_credit_card_query
+      |> Abank.Repo.one()
+
+    if open_invoice do
+      if transaction.amount_in_cents > credit_card.limit_in_cents - open_invoice.amount_in_cents do
+        with {:ok, _result} <-
+               Abank.Repo.transaction(fn ->
+                 transaction
+                 |> Ecto.Changeset.change(status: "rejected")
+                 |> Abank.Repo.update!()
+               end) do
+          {:error, %{result: "Your transaction surpass the credit card limit.", status: 400}}
+        else
+          {:error, result} ->
+            IO.inspect(result)
+            {:error, %{result: "Something bad happened", status: 500}}
+        end
+      else
+        with {:ok, result} <-
+               Abank.Repo.transaction(fn ->
+                 open_invoice
+                 |> Ecto.Changeset.change(
+                   amount_in_cents: open_invoice.amount_in_cents + transaction.amount_in_cents
+                 )
+                 |> Abank.Repo.update!()
+
+                 to_account
+                 |> Ecto.Changeset.change(
+                   balance_in_cents: to_account.balance_in_cents + transaction.amount_in_cents
+                 )
+                 |> Abank.Repo.update!()
+
+                 transaction
+                 |> Ecto.Changeset.change(status: "approved")
+                 |> Abank.Repo.update!()
+               end) do
+          {:ok, %{result: result}}
+        else
+          {:error, result} ->
+            IO.inspect(result)
+            {:error, %{result: "Something bad happened", status: 500}}
+        end
+      end
+    else
+      Handler.verify_credit_card_invoices_status(credit_card)
+    end
   end
 
   def run_transaction(%Transaction{} = transaction) do
