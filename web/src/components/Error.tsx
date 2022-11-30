9905125bfda78a165a670f11035d5eed4aff0dc3
abehidek <hidek.abe@outlook.com>
2022-11-29 23:38:03 -0300

    add: Error component

---
+++ web/src/components/Error.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-xl font-bold">Something wrong happened</h1>
+    </div>
+  );
+}
