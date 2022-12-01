e82838870bb09a785a9d5affe0b7a71649700e3e
abehidek <hidek.abe@outlook.com>
2022-11-30 21:22:39 -0300

    define: theme tokens set in tailwind config file

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
     extend: {
       gridTemplateColumns: {
         main: "1fr 1fr",
+      },
+      fontFamily: {
+        sans: 'Zen Kaku Gothic New, sans-serif'
       }
     },
   },
